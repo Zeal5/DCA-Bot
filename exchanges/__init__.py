00a3fa044efbe15831ebc040501b961639cc2b88
Zeal5 <tassadiqshakeel5@gmail.com>
2024-03-07 22:08:31 +0500

    new type PlacedOrder created

--- exchanges/__init__.py
+++ exchanges/__init__.py
@@ -2,33 +2,43 @@ from dataclasses import dataclass
 from dotenv import load_dotenv
 from typing import Optional
 
+from typing import Literal
+from enum import Enum
 import os
+
 load_dotenv()
 
 GATEIO_KEY = os.environ.get("GATEIO_KEY")
 GATEIO_SECRET = os.environ.get("GATEIO_SECRET")
 
 
-@dataclass 
-class Order:
+@dataclass
+class OrderToBePlaced:
     """Order to be passed to Exchanges
 
     `price :` price of asset `central grid price`
-    `cost :`  units * `price` == cost 
-    `do_not_buy_above_price` & `do_not_buy_below_price` 
+    `cost :`  units * `price` == cost
+    `do_not_buy_above_price` & `do_not_buy_below_price`
 
     high and low ends of grids will be passed to
     GridLine:do_not_buy_below_price
-    GridLine:do_not_buy_above_price and 
+    GridLine:do_not_buy_above_price and
     """
 
     price: float
-    cost : float
+    amount:float
+    currency_pair:str
     do_not_buy_above_price: Optional[float] = None
     do_not_buy_below_price: Optional[float] = None
 
 
-
-
-
-
+@dataclass
+class PlacedOrders:
+    """Order to be returned From Exchanges"""
+    price: float
+    tokens: float
+    order_id: int
+    create_time: int
+    currency_pair: str
+    side: Literal["BUY", "SELL"]
+    success: bool
